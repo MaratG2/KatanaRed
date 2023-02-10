711703e4defcb29cd7d85687ebac182f8d88eb2b
Marat <maratg2develop@gmail.com>
2023-02-10 11:26:58 +0400

    Horizontal Dash data

---
+++ Assets/_Scripts/Utils/Scriptables/HorizontalDashSO.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace KatanaRed.Utils.Scriptables
+{
+    [CreateAssetMenu(fileName = "HorizontalDashData", menuName = "KatanaRed/ScriptableObjects/HorizontalDashData", order = 1)]
+    public class HorizontalDashSO : ScriptableObject
+    {
+        [SerializeField]
+        private float _dashCooldown = 1f;
+        [SerializeField]
+        private Vector2 _dashDirection = Vector2.right;
+        [SerializeField]
+        private float _dashStrength = 1f;
+        [SerializeField] 
+        private float _dashTime = 0.2f;
+        
+        public float DashCooldown => _dashCooldown;
+        public Vector2 DashDirection => _dashDirection;
+        public float DashStrength => _dashStrength;
+        public float DashTime => _dashTime;
+    }
+}
